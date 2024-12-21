11a7147642ba0200954930e7e93166ad84c91666
rcqx <jose@cozey.ca>
2024-12-21 01:32:41 -0500

    update

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -2,9 +2,10 @@ export default function handler(req: any, res: any) {
   console.log('process.env.CLIENT_ID -->', process.env.CLIENT_ID)
   console.log('process.env.REDIRECT_URI -->', process.env.REDIRECT_URI)
 
-  const authorizationUrl = `https://secure.meetup.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(
-    process.env.REDIRECT_URI || '',
-  )}`
+  const encodedClientId = encodeURIComponent(process.env.CLIENT_ID!)
+  const encodedRedirectUri = encodeURIComponent(process.env.REDIRECT_URI!)
+
+  const authorizationUrl = `https://secure.meetup.com/oauth2/authorize?client_id=${encodedClientId}&response_type=code&redirect_uri=${encodedRedirectUri}`
 
   res.redirect(authorizationUrl)
 }
