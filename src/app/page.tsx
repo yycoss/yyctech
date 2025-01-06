f2f0ac5aef394bf0fe31504e7d4cc5a183bf9741
rcqx <jose@cozey.ca>
2025-01-05 19:41:25 -0500

    fetch data every 2 hrs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ async function fetchCommunities() {
     headers: {
       Authorization: `Bearer YOUR_ACCESS_TOKEN`,
     },
-    // next: { revalidate: 7200 }, // revalidate every 2 hours
-    cache: 'no-cache',
+    next: { revalidate: 7200 }, // revalidate every 2 hours
   })
 
   if (!response.ok) {
