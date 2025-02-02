e92bb3799878cdc3f9cebef92912b69a6ce723bf
rcqx <jose@cozey.ca>
2025-02-02 16:45:19 -0500

    fix: markdown rendered font colors

--- src/components/MarkdownRenderer/index.tsx
+++ src/components/MarkdownRenderer/index.tsx
@@ -1,16 +1,22 @@
-import remarkGfm from "remark-gfm";
-import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+import ReactMarkdown from 'react-markdown'
+import './styles.css'
 
 interface MarkdownRendererProps {
-  content: string;
+  content: string
 }
 
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
   return (
     <div className="prose max-w-none">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        className="markdown-content text-zinc-600 dark:text-zinc-300"
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
-  );
-};
+  )
+}
 
-export default MarkdownRenderer;
+export default MarkdownRenderer
